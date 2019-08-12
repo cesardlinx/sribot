81bc9918d20436b50634845d60046ff4211a7073
David Padilla <davidpadilla.f22@gmail.com>
2019-08-12 08:19:50 -0500

    Traverse all pages and months
    The bot traverses all pages in the table to check each invoice
    and also does this for each month

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from selenium import webdriver
+import sys
+from datetime import datetime
 from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
+
+MONTHS = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO',
+          'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE']
 
 
 def main():
@@ -41,42 +47,92 @@ def main():
         (By.ID, 'j_id325:j_id334')))
     facturas_electronicas_link.click()
 
-    # Year selection
-    wait.until(EC.element_to_be_clickable(
-        (By.XPATH,
-            "//select[@name='j_id148:cmbAnio']/option[text()='2019']")))\
-        .click()
+    year = '2019'
+    month = 'ENERO'
+    current_year = datetime.now().year
+    current_month_index = datetime.now().month - 1
+    previous_years = current_year - int(year)
+
+    initial_index = MONTHS.index(month)
+
+    if previous_years:
+        # months in the first year
+        months = MONTHS[initial_index:]
+        for month in months:
+            check_invoices(driver, year, month)
+
+    # months in the actual year
+    months = MONTHS[:current_month_index]
+    year = str(current_year)
+    for month in months:
+        check_invoices(driver, year, month)
+
+    # driver.close()
+    sys.exit()
+
+
+def check_invoices(driver, year, month):
+    wait = WebDriverWait(driver, 10)
+
+    # year selection
+    wait.until(EC.element_to_be_clickable((
+        By.XPATH,
+        "//select[@name='j_id148:cmbAnio']/option[text()='{}']".format(year)
+    ))).click()
 
     # Period selection
     wait.until(EC.element_to_be_clickable(
         (By.XPATH,
-            "//select[@name='j_id148:cmbPeriodo']/option[text()='JULIO']")))\
-        .click()
+            "//select[@name='j_id148:cmbPeriodo']/option[text()='{}']"
+            .format(month)))).click()
+
     wait.until(EC.element_to_be_clickable(
         (By.ID,
             'j_id148:btnBuscarComprobantesElectronicos')))\
         .click()
 
-    table = wait.until(EC.presence_of_element_located(
+    wait.until(EC.presence_of_element_located(
         (By.ID,
-            'j_id148:tblFacturas')))
-    if table:
-        fill_table(driver)
+            'j_id148:tblFacturas:paginadorFactura_table')))
 
+    first_number = driver.find_element_by_xpath(
+        "//table[@id='j_id148:tblFacturas']/tbody/tr/td")
+    first_number = int(first_number.text)
+
+    if first_number > 1:
+        expect_number = first_number - 10
+        id_number = (first_number - 1) - 10
+
+        # go to first page
+        while has_previous_page(driver):
+            go_previous_page(driver)
+            wait.until(EC.text_to_be_present_in_element(
+                (By.ID, 'j_id148:tblFacturas:{}:j_id165'.format(id_number)),
+                str(expect_number)))
+    else:
+        wait.until(EC.text_to_be_present_in_element(
+            (By.ID, 'j_id148:tblFacturas:0:j_id165'), '1'))
+
+    fill_in_table(driver)
+
+    i = 1
     while has_next_page(driver):
         go_next_page(driver)
-        sleep(1)
-        table = wait.until(EC.presence_of_element_located(
-            (By.ID,
-             'j_id148:tblFacturas')))
-        if table:
-            fill_table(driver)
+        # wait until change of cell
+        first_number = i*10 + 1
+        id_number = i*10
+        wait.until(EC.text_to_be_present_in_element(
+            (By.ID, 'j_id148:tblFacturas:{}:j_id165'.format(id_number)),
+            str(first_number)))
 
-    sleep(5)
-    driver.close()
+        fill_in_table(driver)
+        i += 1
+
+    driver.find_element_by_id(
+        'j_id148:btnGuardarFacturasSeleccionadas').click()
 
 
-def fill_table(driver):
+def fill_in_table(driver):
     table_rows = driver.find_elements_by_xpath(
         "//table[@id='j_id148:tblFacturas']/tbody/tr")
 
@@ -96,6 +152,33 @@ def has_next_page(driver):
     next_button = paginator_cells[-2]
     class_name = next_button.get_attribute('class')
     return 'dsbld' not in class_name
+
+
+def has_previous_page(driver):
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH,
+            "//table[@id='j_id148:tblFacturas:paginadorFactura_table']/tbody"
+            "/tr/td")))
+    paginator_cells = driver.find_elements_by_xpath(
+        "//table[@id='j_id148:tblFacturas:paginadorFactura_table']/tbody"
+        "/tr/td")
+    previous_button = paginator_cells[1]
+    class_name = previous_button.get_attribute('class')
+    return 'dsbld' not in class_name
+
+
+def go_previous_page(driver):
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH,
+            "//table[@id='j_id148:tblFacturas:paginadorFactura_table']/tbody"
+            "/tr/td")))
+    paginator_cells = driver.find_elements_by_xpath(
+        "//table[@id='j_id148:tblFacturas:paginadorFactura_table']/tbody"
+        "/tr/td")
+    previous_button = paginator_cells[1]
+    previous_button.click()
 
 
 def go_next_page(driver):
